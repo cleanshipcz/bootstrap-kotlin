03a128d8ef12ac95153dd342e80ad8782da58503
Michael Blaha <blahami2@gmail.com>
2025-08-31 19:15:08 +0200

    Cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     // Apply the shared build logic from a convention plugin.
-    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
     alias(libs.plugins.cleanship.kotlin.convention)
 
     // Apply the Application plugin to add support for building an executable JVM application.
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     // Apply the shared build logic from a convention plugin.
-    // The shared code is located in `buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts`.
     alias(libs.plugins.cleanship.kotlin.convention)
     // Apply Kotlin Serialization plugin from `gradle/libs.versions.toml`.
     alias(libs.plugins.kotlinPluginSerialization)

f30fb798b58de55a9a55d4dd486da27e25139637
Michael Blaha <michael.blaha@sentinelone.com>
2025-09-09 14:27:56 +0200

    [COM-5] remove detekt warning

--- utils/src/main/kotlin/cz/cleanship/utils/Printer.kt
+++ utils/src/main/kotlin/cz/cleanship/utils/Printer.kt
@@ -3,18 +3,23 @@ package cz.cleanship.utils
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.datetime.Clock
-import kotlinx.datetime.Instant
 import kotlinx.serialization.Serializable
+import java.time.Instant
 
 @Serializable
 class Printer(
     val message: String,
 ) {
+
+    /**
+     * Test delay in milliseconds.
+     */
+    private val testDelay = 1000L
+
     fun printMessage() = runBlocking {
-        val now: Instant = Clock.System.now()
+        val now: Instant = Instant.now()
         launch {
-            delay(1000L)
+            delay(testDelay)
             println(now.toString())
         }
         println(message)
